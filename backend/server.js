25f5eb54ef104209bc068ee92cd3be683faf9678
Anita Avdiu <anitaavdiu@Anitas-MacBook-Air.local>
2025-10-08 03:06:11 -0400

    Fix server.js and confirm working locally

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"; 
+import cors from "cors";
 import { connectDB } from './config/db.js';
 import memberRoutes from "./routes/member.route.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
+
 app.use(cors());
 app.use(express.json()); 
 
-app.use("/api/members", memberRoutes);
-
 app.get("/", (req, res) => {
-  res.send("Tufts ASA backend is running successfully!");
+  res.send("✅ Tufts ASA backend is running successfully!");
 });
 
+app.use("/api/members", memberRoutes);
 
 connectDB();
 
