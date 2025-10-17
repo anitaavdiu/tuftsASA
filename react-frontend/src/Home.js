0cca949f475ff4b9f3e6764073bc877fe7f38e0c
Anita Avdiu <anitaavdiu@Anitas-MacBook-Air.local>
2025-10-17 00:04:30 -0400

    Make hero image fully responsive across all devices

--- react-frontend/src/Home.js
+++ react-frontend/src/Home.js
@@ -41,7 +41,8 @@ export default function Home() {
 
         .heroimage {
         width: 100%;
-        height: 100vh;
+        height: 60vh;
+        min-height: 450px;
         background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/dua-alb.jpeg');
         background-size: cover;
         background-position: center;
