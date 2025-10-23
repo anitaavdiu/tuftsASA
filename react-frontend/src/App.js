adae7875982eaf3beaffbb032361b644e72af14c
Anita Avdiu <anitaavdiu@Anitas-MacBook-Air.local>
2025-10-23 18:16:10 -0400

    Fix App.js imports to use correct Home and Join components
    - Update App.js to import from ./Home and ./Join instead of ./pages/
    - Remove Header component import since Home and Join have their own navbars
    - Ensure navbar changes are now properly connected and working

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,16 +1,14 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Join from "./pages/Join";
+import Home from "./Home";
+import Join from "./Join";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/join" element={<Join />} />
-        {}
       </Routes>
     </BrowserRouter>
   );
