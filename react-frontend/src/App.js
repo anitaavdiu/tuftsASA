a02236d0fcb55fbec623e9433a7e318b333acfdf
Anita Avdiu <anitaavdiu@Anitas-MacBook-Air.local>
2025-10-23 18:19:19 -0400

    Switch back to TSX pages for navbar
    - Update App.js to use ./pages/Home and ./pages/Join
    - Use navbar from TSX components instead of embedded navbars
    - Fix duplicate Home issue by using proper component structure

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Join from "./Join";
+import Home from "./pages/Home";
+import Join from "./pages/Join";
 
 function App() {
   return (
